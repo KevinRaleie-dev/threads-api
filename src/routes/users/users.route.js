74e6f4a1723b78c1d75f9d20048b79f291e48fa5
KevinRaleie-dev <kkevraleie@gmail.com>
2020-11-10 18:53:51 +0200

    added create product route, add by logged in user

--- src/routes/users/users.route.js
+++ src/routes/users/users.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { User } from '../../models/User';
+import { Product } from '../../models/Product';
+import {authenticate} from '../../helpers/verifyToken';
 
 const router = express.Router();
 
 // get all the users in the database
-router.get('/', async (_, res) => {
+router.get('/', authenticate, async (_, res) => {
 
     const users = await User.find().then(user => res.json(user)).catch(err => res.status(400).json({
         message: err.message
@@ -13,6 +15,7 @@ router.get('/', async (_, res) => {
     return users;
 });
 
+// get a user by id and all their products
 router.get('/:id', async (req, res) => {
 
     const findUserById = await User.findById( req.params.id ).populate('products')
@@ -24,6 +27,39 @@ router.get('/:id', async (req, res) => {
     return findUserById;
 });
 
+// create a product
+router.post('/create_product', authenticate, async (req, res) => {
+    try {
+        const product = new Product({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            stock: req.body.stock,
+            image: req.body.image,
+            owner: req.loggedInUser._id
+        });
+        await product.save();
+
+        // const getUser = req.loggedInUser._id;
+
+        const user = await User.findById({_id: product.owner});
+        user.products.push(product);
+
+        await user.save();
+
+        res.status(201).json({
+            success: true,
+            data: product
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: `Error: ${error.message}`
+        })
+    }
+});
+
+// update a user by id
 router.patch('/update/:id', async (req, res) => {
     const updateUser = await User.findById(req.params.id).then(user => {
         user.email = req.body.email;
@@ -43,6 +79,7 @@ router.patch('/update/:id', async (req, res) => {
     return updateUser;
 });
 
+// delete a user
 router.delete('/delete/:id', async (req, res) => {
     const deleteUser = await User.findByIdAndDelete(req.params.id)
                         .then(() => {
