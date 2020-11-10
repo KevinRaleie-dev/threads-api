f70b8c66b9e6f98b285221cc2b667cd6186cb155
KevinRaleie-dev <kkevraleie@gmail.com>
2020-11-10 08:31:35 +0200

    added patch and delete routes to the user

--- src/routes/users/users.route.js
+++ src/routes/users/users.route.js
@@ -13,4 +13,48 @@ router.get('/', async (_, res) => {
     return users;
 });
 
+router.get('/:id', async (req, res) => {
+
+    const findUserById = await User.findById( req.params.id ).populate('products')
+                            .then(user => res.json(user))
+                            .catch(err => res.json({
+                                message: err.message
+                            }));
+    
+    return findUserById;
+});
+
+router.patch('/update/:id', async (req, res) => {
+    const updateUser = await User.findById(req.params.id).then(user => {
+        user.email = req.body.email;
+        user.username = req.body.username;
+        user.password = req.body.password;
+
+        user.save().then(() => {
+            res.json({
+                message: 'User updated successfully'
+            })
+        })
+        .catch(err => res.status(400).json({
+            message: `Error: ${err.message}`
+        }));
+    });
+
+    return updateUser;
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    const deleteUser = await User.findByIdAndDelete(req.params.id)
+                        .then(() => {
+                            res.status(200);
+                        })
+                        .catch(err => {
+                            res.status(400).json({
+                                message: `Error: ${err.message}`
+                            })
+                        });
+    return deleteUser;
+})
+
+
 module.exports = router;
