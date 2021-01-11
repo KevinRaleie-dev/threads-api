83ff1067ce40039f3c026a70379758940f34ed96
KevinRaleie-dev <kkevraleie@gmail.com>
2021-01-11 21:03:31 +0200

    Stricter typescript type checking

--- src/utils/inputs/loginInput.ts
+++ src/utils/inputs/loginInput.ts
@@ -1,7 +1,8 @@
 import { Field, InputType } from "type-graphql";
+import { User } from '../../entities/User';
 
 @InputType({description: 'Login user data'})
-export class LoginUserInput {
+export class LoginUserInput implements Partial<User>{
 
     @Field()
     email: string;
--- src/utils/inputs/registerInput.ts
+++ src/utils/inputs/registerInput.ts
@@ -1,7 +1,8 @@
 import { Field, InputType } from "type-graphql";
+import { User } from '../../entities/User';
 
 @InputType({description: 'Register user data'})
-export class RegisterUserInput {
+export class RegisterUserInput implements Partial<User> {
 
     @Field()
     username: string;
